64f02e1c263d3061df59bccd6ecfc63957546e1e
Aisya <aisyanwarputri@gmail.com>
2022-03-25 16:43:59 +0700

    13 Fungsi Overriding

---
+++ src/main/kotlin/id/ac/polbeng/Aisya/test_kelas/InheritanceC.kt
@@ -0,0 +1,24 @@
+package id.ac.polbeng.Aisya.test_kelas
+
+open class Teacher {
+    // Harus menggunakan pengubah "terbuka" untuk mengizinkan kelas anak menimpanya
+    open fun teach() {
+        println("Teaching...")
+    }
+    fun info(){
+        println("I'am a Teacher.")
+    }
+}
+class MathsTeacher : Teacher() {
+    // Harus menggunakan pengubah "override" untuk mengganti fungsi kelas dasar
+    override fun teach() {
+        println("Teaching Maths...")
+    }
+}
+fun main() {
+    val teacher = Teacher()
+    val mathsTeacher = MathsTeacher()
+    mathsTeacher.info()
+    teacher.teach()
+    mathsTeacher.teach()
+}
