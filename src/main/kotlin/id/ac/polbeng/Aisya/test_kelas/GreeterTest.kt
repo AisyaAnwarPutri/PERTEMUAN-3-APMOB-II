14ab226553bcfb520ca4df99d24a419245b6dbff
Aisya <aisyanwarputri@gmail.com>
2022-03-25 11:18:15 +0700

    1 Kelas dan Objek

--- src/main/kotlin/id/ac/polbeng/Aisya/test_kelas/GreeterTest.kt
+++ src/main/kotlin/id/ac/polbeng/Aisya/test_kelas/GreeterTest.kt
@@ -3,4 +3,10 @@ package id.ac.polbeng.Aisya.test_kelas
 fun main() {
     val greeter = Greeter()
     greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Aisya")
+    greeter.greet("Yesi Indriani")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Anwar"))
+
 }
