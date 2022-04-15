c7a210a667545ad526f214dc15ba14f6c5a2621e
AISYA ANWAR PUTRI <aisyanwarputri@gmail.com>
2022-04-15 09:11:36 +0700

    14_Overriding Property

--- src/main/kotlin/id/ac/polbeng/Aisya/test_kelas/InheritanceD.kt
+++ src/main/kotlin/id/ac/polbeng/Aisya/test_kelas/InheritanceD.kt
@@ -3,35 +3,43 @@ package id.ac.polbeng.Aisya.test_kelas
 import java.text.NumberFormat
 import java.util.*
 
-//14
+
+//Sama halnya seperti fungsi, property di Kotlin pada kelas induk dapat di override dengan menggunakan akses modifier open
 open class EmployeeD {
-    // Gunakan pengubah "terbuka" untuk mengizinkan kelas anak menimpa properti ini
+    // Use "open" modifier to allow child classes to override this property
     open val baseSalary: Int = 3000000
 }
+
 class Programmer : EmployeeD() {
-    // Gunakan pengubah "override" untuk mengganti properti kelas dasar
+    // Use "override" modifier to override the property of base class
     override val baseSalary: Int = 5000000
 }
 class SoftwareEngineer : EmployeeD() {
-    // Gunakan pengubah "override" untuk mengganti properti kelas dasar
+    // Use "override" modifier to override the property of base class
     override val baseSalary: Int = 6000000
 }
+
 class ProjectManager : EmployeeD() {
-    // Gunakan pengubah "override" untuk mengganti properti kelas dasar
+    // Use "override" modifier to override the property of base class
     override val baseSalary: Int = 8000000
 }
+
 fun toRupiahFormat(baseSalary: Int) : String{
-    val localeID = Locale("in", "ID")
+    val localeID =  Locale("in", "ID")
     val numberFormat = NumberFormat.getCurrencyInstance(localeID)
     return numberFormat.format(baseSalary).toString()
 }
+
 fun main() {
     val employee = EmployeeD()
-    println("Rata-rata gaji ${employee.javaClass.simpleName} = ${toRupiahFormat((employee.baseSalary))}")
+    println("Rata-rata gaji ${employee.javaClass.simpleName} = ${toRupiahFormat(employee.baseSalary)}")
 
     val programmer = Programmer()
-    println("Rata-rata gaji ${programmer.javaClass.simpleName} = ${toRupiahFormat((programmer.baseSalary))}")
+    println("Rata-rata gaji ${programmer.javaClass.simpleName} = ${toRupiahFormat(programmer.baseSalary)}")
 
     val softwareEngineer = SoftwareEngineer()
     println("Rata-rata gaji ${softwareEngineer.javaClass.simpleName} = ${toRupiahFormat(softwareEngineer.baseSalary)}")
+
+    val projectManager = ProjectManager()
+
 }
